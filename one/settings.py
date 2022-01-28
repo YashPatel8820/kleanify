9f2eccedec24bb67ced937a58b47727e876b9988
YashPatel8820 <yash19959@gmail.com>
2022-01-28 09:16:55 +0530

    sdfghj

--- one/settings.py
+++ one/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = '1#s)6@xsd!uy^(iakv!+(adlv1zekjl(-$6#wtmae9%*88w^*4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['kleanify.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['kleanify.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
